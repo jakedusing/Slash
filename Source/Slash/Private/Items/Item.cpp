86005900607277cbc13bebab1ae2c6b10e40a5f2
Jake Dusing <dusingjake@gmail.com>
2023-12-10 20:01:15 -0600

    initial commit

---
+++ Source/Slash/Private/Items/Item.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+	
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
