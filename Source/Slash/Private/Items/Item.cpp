3fd85c3c75f2c78755e252e684411d6fee648914
Jake Dusing <dusingjake@gmail.com>
2023-12-12 20:31:18 -0600

    add bird pawn

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -9,25 +9,31 @@ AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UWorld* World = GetWorld();
-	FVector Location = GetActorLocation();
-	FVector Forward = GetActorForwardVector();
-	
-	DRAW_SPHERE(Location);
-	// DRAW_LINE(Location, Location + Forward * 100.f);
-	// DRAW_POINT(Location + Forward * 100.f);
-	DRAW_VECTOR(Location, Location + Forward * 100.f);
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
 }
 
