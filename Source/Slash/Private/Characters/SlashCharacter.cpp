831f9a2a863159b464023bf998030dc8fcdd0298
Jake Dusing <dusingjake@gmail.com>
2023-12-18 21:21:54 -0600

    start weapon class

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -119,5 +119,7 @@ void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 	PlayerInputComponent->BindAxis(FName("MoveRight"), this, &ASlashCharacter::MoveRight);
 	PlayerInputComponent->BindAxis(FName("Turn"), this, &ASlashCharacter::Turn);
 	PlayerInputComponent->BindAxis(FName("LookUp"), this, &ASlashCharacter::LookUp);
+
+	PlayerInputComponent->BindAction(FName("Jump"), IE_Pressed, this, &ACharacter::Jump);
 }
 
