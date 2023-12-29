7cd0413d55c5b162dd2dff39b29ff04afe1dd89b
Jake Dusing <dusingjake@gmail.com>
2023-12-28 19:56:49 -0600

    add breakables and start treasure pickup

--- Source/Slash/Private/Items/Weapons/Weapon.cpp
+++ Source/Slash/Private/Items/Weapons/Weapon.cpp
@@ -99,8 +99,9 @@ void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Oth
 		IHitInterface* HitInterface = Cast<IHitInterface>(BoxHit.GetActor());
 		if (HitInterface)
 		{
-			HitInterface->GetHit(BoxHit.ImpactPoint);
+			IHitInterface::Execute_GetHit(BoxHit.GetActor(), BoxHit.ImpactPoint);
 		}
 		IgnoreActors.AddUnique(BoxHit.GetActor());
+		CreateFields(BoxHit.ImpactPoint);
 	}
 }
